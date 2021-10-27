f7b1b4998fede4bcd5b1d0013547ab3dd9e1eeee
Crowdbotics <team@crowdbotics.com>
2021-10-27 22:45:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "dry_cloud_31734",
+  url: "https://dry_cloud_31734.botics.co",
+  api: "https://dry_cloud_31734.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
